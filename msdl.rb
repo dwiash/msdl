e0bff9b1ffb47c02bed996f69df6f47c8e6cdfe4
Patrick Kontschak <patrick.kontschak@gmail.com>
2012-06-16 12:05:20 -0400

    Update for new MangaStream website
    I updated the code to work with the new MangaStream website (2011-2012). The original msdl from dwiash was only working with the old version from 2 years ago.
    
    I also added double-digit file-naming and a gitignore. Everything else works exactly like dwiash's version.
    
    Signed-off-by: Patrick Kontschak <patrick.kontschak@gmail.com>

--- msdl.rb
+++ msdl.rb
@@ -7,20 +7,28 @@ doc = Nokogiri::HTML(open(ARGV[0]))
 Title 	= Array.new(doc.css('head title'))
 title = Title[0].content.partition(" - ")[0]
 dir = title.tr(" ","_")
-Pages 	= Array.new(doc.css('.chaptercontrols select'))
+Pages 	= Array.new(doc.css('#controls a'))
 print "Downloading " + title
 puts `mkdir #{dir}`
-total_page = Pages[1].children.length
+total_page = Pages[1..-2].length
 current = 0
-Pages[1].children.each do |item|
-    current = current + 1
-    page = Nokogiri::HTML(open("http://mangastream.com" + item['value']))
-    element = Array.new(page.css('#pagewrapper #page img#p'))
-    img = element[0]['src']
-    print "File #{current}/#{total_page}"
-    puts `wget -q -c -P #{dir} #{img}`
+Pages[1..9].each do |item|
+	current = current + 1
+	page = Nokogiri::HTML(open("http://mangastream.com/" + item['href']))
+	element = Array.new(page.css('#pagewrapper #page img#p'))
+	img = element[0]['src']
+	print "File #{current}/#{total_page}"
+	puts `wget -q -c -P #{dir} #{img} -O #{dir}/0#{current}.png`
+end
+Pages[10..-2].each do |item|
+	current = current + 1
+	page = Nokogiri::HTML(open("http://mangastream.com/" + item['href']))
+	element = Array.new(page.css('#pagewrapper #page img#p'))
+	img = element[0]['src']
+	print "File #{current}/#{total_page}"
+	puts `wget -q -c -P #{dir} #{img} -O #{dir}/#{current}.png`
 end
 print `tar -cf #{dir}.tar #{dir}/`
 print `bzip2 -z #{dir}.tar`
 print `rm -rf #{dir}`
-puts "Done, Saved to \"#{dir}.tar.bz2\" "
+puts "Done, Saved to \"#{dir}\" "
